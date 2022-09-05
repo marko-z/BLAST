adfe13f416ee65e5541b1f3dfa36a01d8aae2153
marko-z <m.zakrzwsk@gmail.com>
2022-09-05 21:00:21 +0200

    Rudimentary endless scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -11,24 +11,43 @@ const mockFetch = async () => {
 
 function App() {
   const [entries, setEntries] = useState([])
-
+  const [file, setFile] = useState()
   const { ref, inView, entry } = useInView({
     treshold: 0,
   })
 
   useEffect(() => {
-    mockFetch().then(reslist => { 
-      setEntries((entries) => [...entries, ...reslist]) 
+    mockFetch().then(reslist => {
+      setEntries((entries) => [...entries, ...reslist])
     })
-  },[inView])
+  }, [inView])
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const form = new FormData()
+    form.append('image', file)
+
+    fetch("/uploadFile", {
+      method: 'POST',
+      body: form
+    }).then(res => {
+      console.log(res)
+    })
+  }
   
   return (
-    <div>{entries.map((el, index) => {
-      if (index === entries.length-1) {
-        return <img ref={ref} style={{width: "100%", margin: "1rem"}}src={`/static/${el}`} />
-      }
-      return <img style={{width: "100%", margin: "1rem"}}src={`/static/${el}`} />
-    })}</div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input onChange={(e) => setFile(e.target.files[0])} type="file"></input>
+        <button type="submit">Submit</button>
+      </form>
+      <div>{entries.map((el, index) => {
+        if (index === entries.length - 1) {
+          return <img ref={ref} style={{ width: "100%", margin: "1rem" }} src={`/static/${el}`} />
+        }
+        return <img style={{ width: "100%", margin: "1rem" }} src={`/static/${el}`} />
+      })}</div>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -3,15 +3,23 @@ const fs = require('fs')
 const app = express()
 const path = require('path')
 app.use('/static', express.static('static'))
+const multer = require('multer')
+const upload = multer({ dest: 'static/'})
+
 const port = 5001
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
 
 app.get('/getFilenames', (req, res) => {
     const names = fs.readdirSync(path.resolve(__dirname, 'static'))
     res.json(names)
+})
+app.post('/uploadFile', upload.single('image'), (req, res) => {
+    console.log('/uploadFile')
+    console.log(req.file)
+    res.status(200).send()
+})
+app.get('/', (req, res) => {
+    res.send('Hello world')
 })
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
