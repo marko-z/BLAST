7f08992257278a14732e1aa0f2dffaaa73abeb74
marko-z <m.zakrzwsk@gmail.com>
2022-09-05 19:24:08 +0200

    Added json file fetch in frontend, changed proxying to localhost:5001@

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react"
+
+const mockFetch = async () => {
+  const res = await fetch("/static/__credits.json")
+  const jsonres = await res.json()
+  return jsonres
+}
 
 function App() {
+  const [entries, setEntries] = useState([])
+
+  useEffect(() => {
+    mockFetch().then(res => console.log(res))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> Frontend</div>
   );
 }
 
