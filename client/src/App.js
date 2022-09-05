09a951e397fe20e0be3199732975c1ab1c395296
marko-z <m.zakrzwsk@gmail.com>
2022-09-05 20:17:24 +0200

    Infinite scroll take 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react"
+import { useInView } from "react-intersection-observer"
 
 const mockFetch = async () => {
   const res = await fetch("/static/__credits.json")
@@ -12,13 +13,21 @@ const mockFetch = async () => {
 function App() {
   const [entries, setEntries] = useState([])
 
+  const { ref, inView, entry } = useInView({
+    treshold: 0,
+  })
+
   useEffect(() => {
     mockFetch().then(reslist => { 
-      setEntries(reslist) 
+      setEntries((entries) => [...entries, ...reslist]) 
     })
-  },[])
+  },[inView])
+  
   return (
-    <div>{entries.map(el => {
+    <div>{entries.map((el, index) => {
+      if (index === entries.length-1) {
+        return <img ref={ref} style={{width: "100%", margin: "1rem"}}src={`/static/${el.key}.jpg`} />
+      }
       return <img style={{width: "100%", margin: "1rem"}}src={`/static/${el.key}.jpg`} />
     })}</div>
   );
